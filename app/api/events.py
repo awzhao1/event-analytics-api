916dae0605e9582983cb5fcba66fc79a39777666
Allen <54683515+awzhao1@users.noreply.github.com>
2025-12-16 15:19:43 -0500

    Add time filtering to events summary endpoint

--- app/api/events.py
+++ app/api/events.py
@@ -6,6 +6,9 @@ from app.schemas.event import EventCreate
 from sqlalchemy import func
 from app.schemas.analytics import EventSummary
 from typing import List
+from datetime import datetime
+from fastapi import Query
+
 
 
 # creates the router
@@ -36,17 +39,28 @@ def create_event(event: EventCreate, db: Session = Depends(get_db)):
 
     return {"id": db_event.id}
 
+# Events summary with optional time filtering
 @router.get("/summary", response_model=List[EventSummary])
-def event_summary(db: Session = Depends(get_db)):
-    results =(
-        db.query(
-            Event.event_type,
-            func.count(Event.id).label("count")
-        )
-        .group_by(Event.event_type)
-        .all()
+def event_summary(
+    start: datetime | None = Query(default = None),
+    end: datetime | None = Query(default = None),
+    db: Session = Depends(get_db)
+):
+    # Get summary of events
+    query = db.query(
+        Event.event_type,
+        func.count(Event.id).label('count')
     )
 
+    # Filter by timestamps if provided in query
+    if start:
+        query = query.filter(Event.timestamp >= start)
+    if end:
+        query = query.filter(Event.timestamp <= end)
+    
+    
+    results = query.group_by(Event.event_type).all()
+    
     return [
         {"event_type": row.event_type, 'count': row.count}
         for row in results
